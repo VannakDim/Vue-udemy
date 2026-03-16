611c9e5851d8cafa946a2cfd2c6e82f1eb7fe3f6
Vannak Dim <vannakhannah@gmail.com>
2026-03-16 19:10:23 +0700

    61. Component Communication with emitting events

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <script>
-  import TagsInput from "./components/TagsInput.vue";
+import TagsInput from "./components/TagsInput.vue";
 
-  export default{
-    components:{
-      // TagsInput,
-    }
-  }
+export default {
+  data: () => ({
+    jsFrameworks: ["react.js", "vue.js", "angular"],
+    backendFrameworks: [],
+  }),
+  methods: {
+    handleChange(tags) {
+      this.jsFrameworks = [...tags];
+    },
+  },
+};
 </script>
 
 <template>
-  <h1>Hello world</h1><br>
-  <tags-input />
-</template>
+  <h1>Your favorite frameworks</h1>
+  <div>{{ jsFrameworks }}</div>
+  <tags-input :selected-tags="jsFrameworks" 
+    @change="handleChange" />
 
+  <div>{{ backendFrameworks }}</div>
+  <tags-input :selected-tags="backendFrameworks" 
+    @change="backendFrameworks = [...$event]" />
+</template>
