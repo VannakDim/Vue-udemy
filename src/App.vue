63a65b4beb974d20d19059ceebb594d1c3d62a6b
M1 <studio@M1s-Mac-mini.local>
2026-03-16 11:46:30 +0700

    Section 8 - 59.Styling the Tags

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+  import TagsInput from "./components/TagsInput.vue";
+
+  export default{
+    components:{
+      // TagsInput,
+    }
+  }
+</script>
+
+<template>
+  <h1>Hello world</h1><br>
+  <tags-input />
+</template>
+
