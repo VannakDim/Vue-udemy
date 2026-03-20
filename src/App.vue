5dff240d5067542991a4d52f08a9e457f3fb2f23
M1 <studio@M1s-Mac-mini.local>
2026-03-21 06:31:04 +0700

    69. Working with props

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,23 @@
 <script>
-import { computed, reactive, ref, toRef, toRefs, watch, watchEffect } from 'vue';
+import CartItem from './components/cartItem.vue';
+import { ref } from 'vue';
 
 export default {
-  
+  components: { CartItem },
   setup(){
-    const message = ref("Hello")
-    const item = reactive({
-      name : "Khmer Food",
-      price : 12,
-      qty: 1
-    })
-    const decrement =()=> item.qty--
-    const increment =()=> item.qty++
-    const swapProduct = ()=>{
-      item.name = "Product A"
-      item.price = 30
-    }
-    const total = computed(() => item.price * item.qty)
-    const { name, price, qty } = toRefs(item)
-
-    watch(()=> item.qty, ()=>{
-      if(item.qty === 5)
-      alert("You cannot add more item")
-    }, { immediate: true })
-
-    watchEffect(()=>{
-      console.log('Price change: ',item.price )
-    })
-    return {
-      qty,
-      message, 
-      increment,
-      decrement,
-      name,
-      price,
-      total,
-      swapProduct
-
-    }
+    const items = ref([
+      {
+        id: 1,
+        name: "Product X",
+        price: 20,
+        qty: 1
+      }
+    ])
+    return { items }
   }
-};
+}
 </script>
 
 <template>
-  <h1>{{ name }} : {{ price }}</h1>
-  <button @click="swapProduct">Swap product</button>
-  <button @click="price++">Increment price</button>
-
-  <h2>{{ qty }}</h2>
-  <button @click="increment">+</button>
-  <button @click="decrement">-</button>
-
-  <h3>Total: {{ total }}</h3>
+  <CartItem v-for="item in items" :cart-item="item" :key="item.id" />
 </template>
---
+++ src/components/CartItem.vue
@@ -0,0 +1,42 @@
+<script>
+import { computed, reactive, toRefs } from 'vue'
+
+export default {
+    props: {
+        cartItem: {
+            type: Object,
+            required: true
+        }
+    },
+  setup (props) {
+    
+    const item = reactive(props.cartItem)
+
+    const increment = () => item.qty++
+    
+    const decrement = () => item.qty--
+
+    const total = computed(() => item.price * item.qty)
+
+    const { name, price, qty } = toRefs(item)
+
+    return {
+      qty,
+      increment,
+      decrement,
+      name,
+      price,
+      total
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ name }} : {{ price }} : {{ qty }}</h1>
+
+  <button @click="increment">+</button>
+  <button @click="decrement">-</button>
+
+  <h3>Total: {{ total }}</h3>
+</template>
