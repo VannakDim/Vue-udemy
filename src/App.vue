92922a549b05af76ea97276992603a1c2db17c25
M1 <studio@M1s-Mac-mini.local>
2026-03-21 07:06:48 +0700

    72. Composable functions

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,28 @@
 <script>
 import CartItem from './components/cartItem.vue';
+import useCart from './components/useCart';
 import { ref } from 'vue';
 
 export default {
   components: { CartItem },
   setup(){
-    const items = ref([
+    const {items, addItem, removeItem} = useCart()
+
+    addItem(
       {
         id: 1,
         name: "Product X",
         price: 20,
         qty: 1
       }
-    ])
+    )
 
-    const handleRemove = (date) => items.value.splice(0,1)
-    return { items, handleRemove }
+    
+    return { items, removeItem }
   }
 }
 </script>
 
 <template>
-  <CartItem v-for="item in items" :cart-item="item" :key="item.id" @remove="handleRemove" />
+  <CartItem v-for="item in items" :cart-item="item" :key="item.id" @remove="removeItem" />
 </template>
